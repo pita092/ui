3cb30c847c3507d415c32d58a297f924b3fdb45c
pita <154020297+pita092@users.noreply.github.com>
2024-10-03 20:09:08 +0300

    s

--- lua/nvchad/stl/vscode.lua
+++ lua/nvchad/stl/vscode.lua
@@ -23,7 +23,7 @@ M.git = utils.git
 M.lsp_msg = utils.lsp_msg
 M.diagnostics = utils.diagnostics
 M.lsp = utils.lsp
-M.cursor = "%#StText# Ln %l, Col %c  "
+M.cursor = "%#StText# line %l, colon %c  "
 M["%="] = "%="
 
 M.cwd = function()
